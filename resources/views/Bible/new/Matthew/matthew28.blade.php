3ec5960b6f460be2e2519dc963f7eeede7ab1c72
Daniel Htoo <74122831+D-star91@users.noreply.github.com>
2020-11-26 16:05:10 +0630

    matthew28

---
+++ resources/views/Bible/new/Matthew/matthew28.blade.php
@@ -0,0 +1,118 @@
+@extends("Layout.Biblelayout")
+@section("content")
+
+        <div class="main-new">
+        
+        </div>
+      <div class="content-new">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-4">
+              <div class="dropdown">
+                <button class="btn btn-outline-danger dropdown-toggle mt-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  ရှင်မဿဲခရစ်ဝင် အခန်းကြီး
+                </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <div class="card-body">
+                        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                          <div class="btn-group mr-2" role="group" aria-label="First group">
+                            <a href="{{route('mat1')}}" class="btn-g2" role="button" aria-pressed="true">၁</a>
+                            <a href="{{route('mat2')}}" class="btn-g2" role="button" aria-pressed="true">၂</a>
+                            <a href="{{route('mat3')}}" class="btn-g2" role="button" aria-pressed="true">၃</a>
+                            <a href="{{route('mat4')}}" class="btn-g2" role="button" aria-pressed="true">၄</a>
+                            <a href="{{route('mat5')}}" class="btn-g2" role="button" aria-pressed="true">၅</a>
+                            <a href="{{route('mat6')}}" class="btn-g2" role="button" aria-pressed="true">၆</a>
+                            <a href="{{route('mat7')}}" class="btn-g2" role="button" aria-pressed="true">၇</a>
+                            <a href="{{route('mat8')}}" class="btn-g2" role="button" aria-pressed="true">၈</a>
+                            <a href="{{route('mat9')}}" class="btn-g2" role="button" aria-pressed="true">၉</a>
+                            <a href="{{route('mat10')}}" class="btn-g2" role="button" aria-pressed="true">၁၀</a>
+                          </div>
+                        </div>
+                        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                          <div class="btn-group mr-2" role="group" aria-label="First group">
+                            <a href="{{route('mat11')}}" class="btn-g2" role="button" aria-pressed="true">၁၁</a>
+                            <a href="{{route('mat12')}}" class="btn-g2" role="button" aria-pressed="true">၁၂</a>
+                            <a href="{{route('mat13')}}" class="btn-g2" role="button" aria-pressed="true">၁၃</a>
+                            <a href="{{route('mat14')}}" class="btn-g2" role="button" aria-pressed="true">၁၄</a>
+                            <a href="{{route('mat15')}}" class="btn-g2" role="button" aria-pressed="true">၁၅</a>
+                            <a href="{{route('mat16')}}" class="btn-g2" role="button" aria-pressed="true">၁၆</a>
+                            <a href="{{route('mat17')}}" class="btn-g2" role="button" aria-pressed="true">၁၇</a>
+                            <a href="{{route('mat18')}}" class="btn-g2" role="button" aria-pressed="true">၁၈</a>
+                            <a href="{{route('mat19')}}" class="btn-g2" role="button" aria-pressed="true">၁၉</a>
+                            <a href="{{route('mat20')}}" class="btn-g2" role="button" aria-pressed="true">၂၀</a>
+                          </div>
+                        </div>
+                        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                          <div class="btn-group mr-2" role="group" aria-label="First group">
+                            <a href="{{route('mat21')}}" class="btn-g2" role="button" aria-pressed="true">၂၁</a>
+                            <a href="{{route('mat22')}}" class="btn-g2" role="button" aria-pressed="true">၂၂</a>
+                            <a href="{{route('mat23')}}" class="btn-g2" role="button" aria-pressed="true">၂၃</a>
+                            <a href="{{route('mat24')}}" class="btn-g2" role="button" aria-pressed="true">၂၄</a>
+                            <a href="{{route('mat25')}}" class="btn-g2" role="button" aria-pressed="true">၂၅</a>
+                            <a href="{{route('mat26')}}" class="btn-g2" role="button" aria-pressed="true">၂၆</a>
+                            <a href="{{route('mat27')}}" class="btn-g2" role="button" aria-pressed="true">၂၇</a>
+                            <a href="{{route('mat28')}}" class="btn-g2" role="button" aria-pressed="true">၂၈</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="new-title">
+                <h3>ရှင်မဿဲခရစ်ဝင် - ၂၈</h3>
+              </div>
+          </div>
+          </div>
+          </div>  
+          <div class="row justify-content-center">
+                  <div class="col-md-1">
+                      <div class="BB">
+                        <a class="btn-bib" href="{{route('mat27')}}" role="button"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-double-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                            <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                          </svg></a>
+                      </div>
+                  </div>              
+                  <div class="col-md-8 p-cont">
+                    <h4>အခန်းကြီး - ၂၈</h4>
+                        <p class="text-justify">
+                            <h6>၁ </h6>ဥ​ပုသ်​နေ့​လွန်၍ ခု​နစ်​ရက်​တွင် ပ​ထ​မ​နေ့​ရက် မိုး​လင်း​အံ့​ဆဲ​ဆဲ​အ​ချိန်၌၊ မာ​ဂ​ဒ​လ​မာ​ရိ​နှင့် အ​ခြား​သော မာ​ရိ​သည် သင်္ချိုင်း​တော်​ကို အ​ကြည့်​အ​ရှု​သွား​ကြ၏။- <br>
+                            <h6>၂ </h6>ထို​အ​ခါ မြေ​ကြီး​သည် ပြင်း​စွာ​လှုပ်​လေ၏။ အ​ကြောင်း​မူ​ကား၊ ထာ​ဝ​ရ​ဘု​ရား၏ ကောင်း​ကင်​တ​မန်​သည် ကောင်း​ကင်​ဘုံ​မှ ဆင်း​လာ၍၊ တွင်း​ဝ၌​ပိတ်​သော​ကျောက်​ကို လှိမ့်​လှန်​ပြီး​လျှင်၊ ထို​ကျောက်​ပေါ်​မှာ ထိုင်​နေ၏။-<br>
+                            <h6>၃ </h6>သူ၏​မျက်​နှာ​သည် လျှပ်​စစ်​ကဲ့​သို့​ထင်၏။ အ​ဝတ်​သည်​လည်း မိုး​ပွင့်​ကဲ့​သို့ ဖြူ၏။-<br>
+                            <h6>၄ </h6>လူ​စောင့်​တို့​သည် ကောင်း​ကင်​တ​မန်​ကို ကြောက်​ရွံ့​သ​ဖြင့် တုန်​လှုပ်၍ သေ​မ​တတ်​ဖြစ်​ကြ၏။-<br>
+                            <h6>၅ </h6>ကောင်း​ကင်​တ​မန်​က​လည်း၊ ကြောက်​ရွံ့​ခြင်း မ​ရှိ​ကြ​နှင့်။ လက်​ဝါး​ကပ်​တိုင်​မှာ​သတ်​သော ယေ​ရှု​ကို သင်​တို့​ရှာ​ကြ​သည်​ကို ငါ​သိ၏။-<br>
+                            <h6>၆ </h6>ဤ​အ​ရပ်၌ မ​ရှိ။ မိန့်​တော်​မူ​သည်​အ​တိုင်း ထ​မြောက်​တော်​မူ​ပြီ။ လာ​ကြ၊ သ​ခင်​လျောင်း​တော်​မူ​ရာ​အ​ရပ်​ကို ကြည့်​ကြ​လော့။-<br>
+                            <h6>၇ </h6>အ​လျင်​အ​မြန်​ပြန်​သွား၍ သေ​ခြင်း​မှ ထ​မြောက်​တော်​မူ​ကြောင်း​ကို တ​ပည့်​တော်​တို့​အား ပြော​ကြ​လော့။ သင်​တို့​ရှေ့ ဂါ​လိ​လဲ​ပြည်​သို့ ကြွ​တော်​မူ​မည်။ ထို​ပြည်၌ သင်​တို့​သည် ကိုယ်​တော်​ကို တွေ့​မြင်​ရ​ကြ​လိမ့်​မည်၊ ငါ​ပြော​ပြီ​ဟု ထို​မိန်း​မ​တို့​အား ကောင်း​ကင်​တ​မန်​ဆို၏။-<br>
+                            <h6>၈ </h6>ထို​မိန်း​မ​တို့​သည်​လည်း ကြောက်​ရွံ့​သ​ဖြင့်​လည်း​ကောင်း၊ အ​လွန်​ဝမ်း​မြောက်​သ​ဖြင့်​လည်း​ကောင်း၊ သင်္ချိုင်း​တော်​မှ အ​လျင်​အ​မြန်​သွား၍ တ​ပည့်​တော်​တို့​အား ထို​သ​တင်း​စ​ကား​ကို ကြား​ပြော​ခြင်း​ငှာ ပြေး​ကြ၏။-<br>
+                            <h6>၉ </h6>လမ်း​မှာ​သွား​ကြ​စဉ် ယေ​ရှု​သည် ကြို​ဆို​တော်​မူ၍၊ သင်​တို့​အား မင်္ဂ​လာ​ဖြစ်​စေ​သ​တည်း​ဟု မိန့်​တော်​မူ၏။ ထို​မိန်း​မ​တို့​သည် ချဉ်း​ကပ်၍ ခြေ​တော်​ကို​ဖက်​ယမ်း​လျက် ပျပ်​ဝပ်​ကိုး​ကွယ်​ကြ၏။- <br>
+                            <h6>၁၀ </h6>ယေ​ရှု​က​လည်း ကြောက်​ရွံ့​ခြင်း​မ​ရှိ​ကြ​နှင့်။ သွား​ကြ​လော့။ ငါ့​ညီ​တို့​သည် ဂါ​လိ​လဲ​ပြည်​သို့ သွား​စေ​ခြင်း​ငှာ ဤ​သ​တင်း​စ​ကား​ကို ကြား​ပြော​ကြ​လော့။ ထို​ပြည်၌ သူ​တို့​သည် ငါ့​ကို တွေ့​မြင်​ရ​ကြ​လိမ့်​မည်​ဟု မိန့်​တော်​မူ၏။<br>
+                            
+                            <h6>၁၁ </h6>ထို​မိန်း​မ​တို့​သည် သွား​ကြ​စဉ်​အ​ခါ လူ​စောင့်​အ​ချို့​တို့​သည် မြို့​ထဲ​သို့​ဝင်၍ ဖြစ်​လေ​သ​မျှ​သော အ​ခြင်း​အ​ရာ​တို့​ကို ယဇ်​ပု​ရော​ဟိတ်​အ​ကြီး​တို့​အား ပြန်​ကြား​ကြ၏။-<br>
+                            <h6>၁၂ </h6>ယဇ်​ပု​ရော​ဟိတ်​အ​ကြီး​တို့​နှင့် လူ​အ​ကြီး​အ​ကဲ​တို့​သည် စု​ဝေး၍ တိုင်​ပင်​ပြီး​မှ စစ်​သူ​ရဲ​တို့​အား ငွေ​အ​များ​ကို ပေး၍၊- <br>
+                            <h6>၁၃ </h6>ည​အ​ခါ ငါ​တို့​အိပ်​ပျော်​စဉ်​တွင် ထို​သူ၏ တ​ပည့်​တို့​သည်​လာ၍ အ​လောင်း​ကို ခိုး​သွား​ကြ​သည်​ဟု သင်​တို့​ပြော​ကြ​လော့။- <br>
+                            <h6>၁၄ </h6>ထို​အ​ကြောင်း​ကို မြို့​ဝန်​မင်း​ကြား​ပြန်​လျှင်၊ ငါ​တို့​သည် သူ့​ကို​ဖျောင်း​ဖျ၍ သင်​တို့၌ အ​မှု​မ​ရှိ​စေ​ခြင်း​ငှာ ငါ​ပြု​မည်​ဟု၊-<br>
+                            <h6>၁၅ </h6>သွန်​သင်​သည်​နှင့်​အ​ညီ စစ်​သူ​ရဲ​တို့​သည် ငွေ​ကို​ယူ၍​ပြု​ကြ၏။ ထို​စ​ကား​ကို ယ​ခု​တိုင်​အောင် ယု​ဒ​လူ​တို့​သည် အ​နှံ့​အ​ပြား ပြော​ဆို​ကြ၏။ <br>
+                            
+                            <h6>၁၆ </h6>တစ်​ကျိပ်​တစ်​ပါး​သော တ​ပည့်​တော်​တို့​သည် ဂါ​လိ​လဲ​ပြည်​သို့ သွား၍ ယေ​ရှု​မှာ​ထား​တော်​မူ​သော တောင်​ပေါ်​သို့ တက်​ကြ​လျှင်၊ ကိုယ်​တော်​ကို​တွေ့​မြင်၍ ပျပ်​ဝပ်​ကိုး​ကွယ်​ကြ၏။-<br>
+                            <h6>၁၇ </h6>အ​ချို့​တို့​သည် ယုံ​မှား​ခြင်း​ရှိ​ကြ၏။- <br>
+                            <h6>၁၈ </h6>ယေ​ရှု​သည် အ​နီး​အ​ပါး​သို့ ချဉ်း​တော်​မူ​ပြီး​လျှင်၊ ကောင်း​ကင်​ဘုံ၌​လည်း​ကောင်း၊ မြေ​ကြီး​ပေါ်၌​လည်း​ကောင်း၊ စီ​ရင်​ပိုင်​သော အ​ခွင့်​တန်​ခိုး​ရှိ​သ​မျှ​တို့​ကို ငါ​ခံ​ရ​ပြီ။-<br>
+                            <h6>၁၉ </h6>သင်​တို့​သွား၍ လူ​မျိုး​တ​ကာ​တို့​ကို ငါ့​တ​ပည့် ဖြစ်​စေ​လျက်၊ ခ​မည်း​တော်၊ သား​တော်၊ သန့်​ရှင်း​သော​ဝိ​ညာဉ်​တော်၏​နာ​မ၌ ဗ​တ္တိ​ဇံ​ကို ပေး​ကြ​လော့။- <br>
+                            <h6>၂၀ </h6>ငါ​သည် သင်​တို့​အား ပေး​သ​မျှ​သော ပ​ညတ်​တော်​တို့​ကို စောင့်​ရှောက်​စေ​ခြင်း​ငှာ ဆုံး​မ​ဩ​ဝါ​ဒ​ပေး​ကြ​လော့။ ငါ​သည်​လည်း ကပ်​က​မ္ဘာ​ကုန်​သည်​တိုင်​အောင် သင်​တို့​နှင့်​အ​တူ အ​စဉ်​မ​ပြတ် ရှိ​သည်​ဟု တ​ပည့်​တော်​တို့​အား မိန့်​တော်​မူ၏။<br>
+                        </p>
+                        <p class="text-center font-weight-bold">ရှင်မဿဲခရစ်ဝင်ပြီး၏။</p>
+                  </div>
+                  <div class="col-auto">
+                    <div class="BF">
+                        <a class="btn-bib" href="{{route('mark1')}}" role="button"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-double-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
+                            <path fill-rule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
+                          </svg></a>
+                    </div>
+                </div>
+          </div>
+          
+        </div>
+        </div>
+      </div>  
+@endsection
