d2c3531ecd6991a0052e046abc4d9456cd32bd24
Daniel Htoo <74122831+D-star91@users.noreply.github.com>
2020-11-28 14:47:20 +0630

    index biblestudy

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -155,6 +155,53 @@
 <!-- Card deck -->
             </div><hr>
             <div class="row">
+              <!-- News jumbotron -->
+<div class="jumbotron text-center hoverable p-4">
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+      <!-- Featured image -->
+      <div class="view overlay">
+        <img src="{{asset('Image/Lesson.png')}}" class="img-fluid" alt="Sample image for first version of blog listing">
+        <a>
+          <div class="mask rgba-white-slight"></div>
+        </a>
+      </div>
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-md-7 text-md-left ml-3 mt-3">
+
+      <!-- Excerpt -->
+      <a href="" class="green-text">
+        <h6 class="h6 pb-1"><i class="fas fa-bible pr-1"></i>Lesson</h6>
+      </a>
+
+      <h4 class="h4 mb-4">Bible Study</h4>
+
+      <p class="font-weight-normal">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque, totam
+        rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+        dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur.</p>
+      <p class="font-weight-normal">by <a><strong>Carine Fox</strong></a>, 19/08/2016</p>
+
+      <a class="btn btn-success" href="">Read more</a>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</div>
+<!-- News jumbotron -->
+            </div><hr>
+            <div class="row">
               <!--Section: Testimonials v.1-->
 <section class="section pb-3 text-center">
 
@@ -267,10 +314,10 @@
 
 </section>
 <!--Section: Testimonials v.1-->
-            </div>
+            </div><hr>
           </div>
         </div>
 
         
-    .
+    
 @endsection
