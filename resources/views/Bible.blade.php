fbe83cdb6f24cbd0c608320d5b22bb8a2ae33d6e
Daniel Htoo <74122831+D-star91@users.noreply.github.com>
2020-12-01 08:04:56 +0630

    luke1

--- resources/views/Bible.blade.php
+++ resources/views/Bible.blade.php
@@ -2214,7 +2214,7 @@
                               <div class="modal-dialog modal-lg">
                                 <div class="modal-content">
                                   <div class="modal-header">
-                                    <h5 class="modal-title text-default" id="LukeLabel">ရှင်လုကာခရစ်ဝင်/h5>
+                                    <h5 class="modal-title text-default" id="LukeLabel">ရှင်လုကာခရစ်ဝင်</h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                       <span aria-hidden="true">&times;</span>
                                     </button>
